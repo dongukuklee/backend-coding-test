abaac36b4dabc4e0875f8a11dc235c78df7a9ec3
lee <xehddnr@naver.com>
2021-12-19 10:51:19 +0900

    chore: logger추가

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const logger = require("morgan");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(require("./controllers"))
+app.use(logger("dev"));
+app.use(require("./controllers"));
 
 app.listen(5000, () => {
   console.log("server...");
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
 const router = Router();
-const stores = require('./stores')
+const stores = require("./stores");
+const geo = require("./geo");
 
-router.get("/stores",stores.getStores)
-router.get("/stores/:name",stores.searchStores)
+router.get("/stores", stores.getStores);
+router.get("/stores/:name", stores.searchStores);
+router.get("/stores/:postcode/:radius", stores.getNearbyStore);
+
+router.get("/geo/:postcode", geo.getGeoResult);
+router.get("/geo/bulk/:stores", geo.getBulkGeoResult);
 
 module.exports = router;
